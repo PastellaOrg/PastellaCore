1bcd3dcfd4232b59dbd843b16e0f4658f3e1b94d
PastellaPNG <pastellanetwork@proton.me>
2026-03-01 13:03:26 +0000

    build(config): enable mingw static linking

--- cmake/mingw_toolchain.cmake
+++ cmake/mingw_toolchain.cmake
@@ -5,3 +5,7 @@ set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Static linking for Windows builds - eliminates need for DLLs like libwinpthread-1.dll
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static")
